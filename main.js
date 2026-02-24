893a00afec3f915c369bb87e7670f82785734dca
MATHEUS HENRY DOS SANTOS <25204051@dsn127m1242374.cfp127aluno.local>
2026-02-24 16:28:06 -0300

    Finalizada atividade

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 "use strict"
 
 function adicionarProduto(){
+    const codigo = document.getElementById("codigo").value
     const produto = document.getElementById("produto").value
+    const quantidade = document.getElementById("quantidade").value
+
     const lista = document.getElementById("lista")
-    const span = document.createElement("soan")
 
-    if(produto == ""){
-        alert("A caixa de entrada está vasia")
+    if(codigo == "" || produto == "" || quantidade == ""){
+        alert("Alguma caixa de entrada está vazia")
     }else{
-        span.textContent = produto
-        span.className = "bg-blue-200 px-8 py-2 border-2 border-black rounded"
+        const linha = document.createElement("div")
+        linha.className = "flex gap-2"
 
-        lista.appendChild(span)
+        const span1 = document.createElement("span")
+        const span2 = document.createElement("span")
+        const span3 = document.createElement("span")
+
+        span1.textContent = codigo
+        span2.textContent = quantidade
+        span3.textContent = produto
+
+        span1.className = "bg-blue-200 px-8 py-2 border-2 border-black rounded"
+        span2.className = "bg-blue-200 px-8 py-2 border-2 border-black rounded"
+        span3.className = "bg-blue-200 px-8 py-2 border-2 border-black rounded"
+
+        linha.appendChild(span1)
+        linha.appendChild(span2)
+        linha.appendChild(span3)
+
+        lista.appendChild(linha)
+
+        limparInput()
     }
 }
 
 function limparInput(){
     document.getElementById("produto").value = ""
+    document.getElementById("codigo").value = ""
+    document.getElementById("quantidade").value = ""
 }
