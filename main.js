f5b950a0549319a794e3eb660d5ba3485108c6c7
MATHEUS HENRY DOS SANTOS <25204051@dsn127m1242374.cfp127aluno.local>
2026-02-24 15:41:15 -0300

    Conclui os dois desafios passados

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 "use strict"
 
 function adicionarProduto(){
-    const produto = document.getElementById('produto')
-    const lista = document.getElementById('lista')
+    const produto = document.getElementById("produto").value
+    const lista = document.getElementById("lista")
+    const span = document.createElement("soan")
 
-    const span = document.createElement('soan')
-    span.textContent = produto.value
-    span.className = 'bg-blue-200 px-8 py-2'
+    if(produto == ""){
+        alert("A caixa de entrada está vasia")
+    }else{
+        span.textContent = produto
+        span.className = "bg-blue-200 px-8 py-2 border-2 border-black rounded"
 
-    lista.appendChild(span)
-
+        lista.appendChild(span)
+    }
 }
 
+function limparInput(){
+    document.getElementById("produto").value = ""
+}
